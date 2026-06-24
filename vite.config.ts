0059591eafffab5b731eccd1782d52df8513f39d
BioArth <62256025+BioArth@users.noreply.github.com>
2026-06-24 10:43:56 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgLoader from 'vite-svg-loader';
 // https://vitejs.dev/config/
 export default defineConfig((): UserConfig => {
   return {
-    base: '',
+    base: '/<REPO>/',
     plugins: [
       vue(),
       svgLoader({
