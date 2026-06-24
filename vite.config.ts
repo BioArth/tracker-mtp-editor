0512d31fe38df42c58fd30557ed4ee0c282528fc
BioArth <62256025+BioArth@users.noreply.github.com>
2026-06-24 10:45:22 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgLoader from 'vite-svg-loader';
 // https://vitejs.dev/config/
 export default defineConfig((): UserConfig => {
   return {
-    base: '/<REPO>/',
+    base: '/tracker-mtp-editor/',
     plugins: [
       vue(),
       svgLoader({
